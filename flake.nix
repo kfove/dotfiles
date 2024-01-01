93cd1644732a8d3b65225bba086a7c27f846a9e4
dingduck <2200310915@mails.guet.edu.cn>
2024-01-01 16:43:57 +0800

    add hm

--- flake.nix
+++ flake.nix
@@ -5,12 +5,18 @@
 
   inputs = {
     # ......
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+	};
 
     # helix editor, use tag 23.10
     helix.url = "github:helix-editor/helix/23.10";
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       nixosdk = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -20,6 +26,12 @@
         specialArgs = inputs;
         modules = [
           ./nixos/configuration.nix
+	  home-manager.nixosModules.home-manager
+	  {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.dingduck = import ./home/home.nix;
+          }
         ];
       };
     };
