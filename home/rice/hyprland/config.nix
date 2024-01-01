306f03118bce3e5c085378a2138528b7fd15f7cf
dingduck <2200310915@mails.guet.edu.cn>
2024-01-01 16:59:48 +0800

    home 2 hypr

---
+++ home/rice/hyprland/config.nix
@@ -0,0 +1,11 @@
+{ config, lib, ... }:
+
+{
+  wayland.windowManager.hyprland = {
+    settings = {{
+      "$MOD" = "SUPER";
+      bind = [
+        "$MOD, RETURN, exec, alacritty"
+	"$MOD, SPACE, exec, wofi --show drun"
+	"$MOD, Q, killactive"
+      ];
---
+++ home/rice/hyprland/default.nix
@@ -0,0 +1,7 @@
+{ pkgs, inputs, ... }:
+
+{
+  imports = [ ./config.nix ];
+  wayland.windowManager.hyprland.enable = true;
+}
+
