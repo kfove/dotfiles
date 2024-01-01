d537fc1e45c901da181ca82b2e271c74622eb594
dingduck <2200310915@mails.guet.edu.cn>
2024-01-01 16:06:50 +0800

    flake init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "NixOS configuration of Ryan Yin";
+
+  # ......
+
+  inputs = {
+    # ......
+
+    # helix editor, use tag 23.10
+    helix.url = "github:helix-editor/helix/23.10";
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    nixosConfigurations = {
+      nixosdk = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        # 将所有 inputs 参数设为所有子模块的特殊参数，
+        # 这样就能直接在子模块中使用 inputs 中的 helix 了
+        specialArgs = inputs;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
